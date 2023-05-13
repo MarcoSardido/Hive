b1bc7a938f9f034b67a4aefe7a509b225ae32024
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-05-13 22:19:02 +0800

    [Todo] create validation before going to home page

---
+++ middlewares/auth.ts
@@ -0,0 +1,23 @@
+/**
+ * This middleware will run every route
+ */
+// export default defineEventHandler(async event => {
+//     const { useAuthUser, useAuthLoading } = useAuth()
+//     const isLoading = useAuthLoading()
+//     const user = useAuthUser()
+
+//     // Todo: Add router guard for home page
+//     console.log('middleware run')
+//     // onBeforeRouteLeave(() => {
+//     //     console.log(user.value)
+//     // })
+//     // watch(isLoading, () => {
+//     //     if (!isLoading.value) {
+//     //         if (to.path === '/auth') return navigateTo('/')
+//     //     }
+//     // })
+
+    
+
+
+// })
